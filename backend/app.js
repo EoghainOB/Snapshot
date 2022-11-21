c8d433124718e870c47b1dc08c9dd19329b9d506
salt-dev <alexander.cheban@gmail.com>
2022-11-21 12:50:54 +0100

    s

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,9 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/build')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'))
-  })
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+//   })
 
 app.get('/api/hello', (req, res) => {
     Users.create({id: "1", name: "Nastaran", email:"n@gmail.com"})
