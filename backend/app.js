ce5ccfe57cf6401d7655d7b3c9d855a1d21473f0
salt-dev <alexander.cheban@gmail.com>
2022-11-21 11:44:03 +0100

    i

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
 app.get('/api/hello', (req, res) => {
     Users.create({id: "1", name: "Nastaran", email:"n@gmail.com"})
