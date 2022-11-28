d3e7cce26d2a5ebadefdca83f6be785b2d528a30
salt-dev <alexander.cheban@gmail.com>
2022-11-28 19:30:31 +0100

    sss

--- backend/app.js
+++ backend/app.js
@@ -205,6 +205,6 @@ const PORT = process.env.PORT || 8000;
   console.log(`Server started on port ${PORT}`);
 });
 
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
   console.log("SERVER RUNNING");
 });
